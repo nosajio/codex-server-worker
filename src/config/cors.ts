f2ccd40cef9aefb993e5e3681988573d07cd7821
Jason Howmans <jason@nosaj.io>
2019-10-07 23:09:26 +0100

    Handle CORS

---
+++ src/config/cors.ts
@@ -0,0 +1,5 @@
+export const corsURLs: string[] = ['localhost:3000', 'nosaj.io'];
+
+export const corsHeaders: string[] = [];
+
+export const corsMethods: string[] = ['GET', 'OPTIONS'];
--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,13 @@
 import { fixUTF8Encoding } from './utils/encoding';
 import { getAllPostsFromStore } from './utils/store';
 import Router from './utils/router';
+import { corsURLs, corsMethods } from './config/cors';
 
 export async function handleRequest(event: FetchEvent): Promise<Response> {
   const posts: PostFile[] = await getAllPostsFromStore(codex_store);
   const router = new Router();
   const cache = caches.default;
+  const origin = event.request.headers.get('Origin');
 
   router.get('/', req => {
     return new Response(
@@ -14,11 +16,13 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
   });
 
   router.get('/posts', async req => {
-    // Check cache for posts response first
     let res = await cache.match(req);
+
+    // Check cache for posts response first
     if (res) {
       return res;
     }
+
     // When cached version isn't available, serve k/v posts
     res = new Response(fixUTF8Encoding(JSON.stringify(posts)));
     return res;
@@ -29,8 +33,9 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
       return new Response('Need params', { status: 404 });
     }
 
-    // Check Cache for cached response
     let res = await cache.match(req);
+
+    // Check Cache for cached response
     if (res) {
       return res;
     }
@@ -38,15 +43,35 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
     // When cached post isn't available, return k/v stored version
     const { slug } = params;
     const post = posts.find(p => p.slug === slug);
+
     if (!post) {
       return new Response(`The post ${slug} cannot be found`, { status: 404 });
     }
+
     res = new Response(fixUTF8Encoding(JSON.stringify(post)));
     return res;
   });
 
   const response = await router.handleRoute(event.request);
+
+  // Set content type header so that clients don't confuse the response with
+  // plain text
   response.headers.set('content-type', 'application/json');
+
+  // Handle cors headers for all requests that aren't preflight (OPTIONS)
+  // requests
+  if (origin && event.request.method !== 'OPTIONS') {
+    const originURL = new URL(origin);
+    const foundHost = corsURLs.find(c => c === originURL.host);
+    if (!foundHost) {
+      return response;
+    }
+    response.headers.set('Access-Control-Allow-Origin', `${originURL.protocol}//${foundHost}`);
+    response.headers.set(
+      'Access-Control-Allow-Methods',
+      corsMethods.join(', '),
+    );
+  }
 
   return response;
 }
